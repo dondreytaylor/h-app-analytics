98fe11b0fbb405ec398dbbba18193b28237470a4
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-31 14:06:15 -0500

    Linked test certs

--- proxy.js
+++ proxy.js
@@ -1,10 +1,11 @@
 var redbird = new require('redbird')({
   	port: 80,
-  	ssl: {
-  		port: 443,
-  		key: "ssl/s.haystack.ai.key",
-  		cert: "./ssl/s.haystack.ai.cer",
-  	}
+    secure: true,
+    ssl: {
+        port: 443,
+        key: "./ssl/_.bithereum.network_private_key.key",
+        cert: "./ssl/bithereum.network_ssl_certificate.cer",
+    }
 });
 
 redbird.register('analytics.bithereum.network', 'http://localhost:8000', {ssl: true});
